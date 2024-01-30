fda5f9af1a6eea637c804b15710e53cd3a499d1f
cinti.av <cin899a@gmail.com>
2024-01-30 15:03:24 -0300

    TODO

---
+++ invitacionAgus/src/assets/maps.jsx
@@ -0,0 +1,21 @@
+// import { GoogleMap, LoadScript } from '@react-google-maps/api'
+// const containerStyle = {
+//   width: '400px',
+//   height: '400px'
+// }
+
+// const center = {
+//   lat: -34.603722,
+//   lng: -58.381592
+// }
+// function Map() {
+//   return (
+//     <LoadScript googleMapsApiKey='YOUR_API_KEY'>
+//       <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
+//         {/* Child components, such as markers, info windows, etc. */}
+//       </GoogleMap>
+//     </LoadScript>
+//   )
+// }
+
+// export default Map
