e046770e6662fd8fcc737521a40813044999ea3c
cinti.av <cin899a@gmail.com>
2024-02-01 21:20:51 -0300

    subiendo cositas

--- invitacionAgus/src/assets/maps.jsx
+++ invitacionAgus/src/assets/maps.jsx
@@ -1,21 +1,27 @@
-// import { GoogleMap, LoadScript } from '@react-google-maps/api'
-// const containerStyle = {
-//   width: '400px',
-//   height: '400px'
-// }
+// Initialize and add the map
+let map
 
-// const center = {
-//   lat: -34.603722,
-//   lng: -58.381592
-// }
-// function Map() {
-//   return (
-//     <LoadScript googleMapsApiKey='YOUR_API_KEY'>
-//       <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
-//         {/* Child components, such as markers, info windows, etc. */}
-//       </GoogleMap>
-//     </LoadScript>
-//   )
-// }
+async function initMap() {
+  // The location of Uluru
+  const position = { lat: -25.344, lng: 131.031 }
+  // Request needed libraries.
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary('maps')
+  const { AdvancedMarkerView } = await google.maps.importLibrary('marker')
 
-// export default Map
+  // The map, centered at Uluru
+  map = new Map(document.getElementById('map'), {
+    zoom: 4,
+    center: position,
+    mapId: 'DEMO_MAP_ID'
+  })
+
+  // The marker, positioned at Uluru
+  const marker = new AdvancedMarkerView({
+    map: map,
+    position: position,
+    title: 'Uluru'
+  })
+}
+
+initMap()
